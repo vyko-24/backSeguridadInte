4c7ab5e59cc351d332a230857cebac3c7ce61df7
vyko-24 <20223tn008[200~ajos>
2025-03-24 20:53:18 -0600

    mamadas pal docker

--- basicAuth8C/src/main/java/utez/edu/mx/basicauth8c/modules/auth/SignedDto.java
+++ basicAuth8C/src/main/java/utez/edu/mx/basicauth8c/modules/auth/SignedDto.java
@@ -1,13 +1,24 @@
 package utez.edu.mx.basicauth8c.modules.auth;
 
+import lombok.*;
 import lombok.Value;
 import utez.edu.mx.basicauth8c.modules.rol.Rol;
 import utez.edu.mx.basicauth8c.modules.user.User;
 
-@Value
+@Getter
+@Builder
+@NoArgsConstructor
 public class SignedDto {
     String token;
     String tokenType;
     User user;
     Rol role;
+
+    public SignedDto(String token, String tokenType, User user, Rol role) {
+        this.token = token;
+        this.tokenType = tokenType;
+        this.user = user;
+        this.role = role;
+    }
+
 }
